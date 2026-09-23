26bcb0aafd9fe3bb96c398bf56cd740c2713b695
Tharun07-9 <tharunjtharunj1@gmail.com>
2026-09-23 17:16:57 +0530

    Modify test case for count_odds function
    Updated expected output for count_odds test case.

--- test_app.py
+++ test_app.py
@@ -14,7 +14,7 @@ def test_find_min(inputs, expected):
 
 @pytest.mark.parametrize("inputs, expected", [
     ([1, 2, 3, 4, 5], 3),    
-    ([2, 4, 6], 0),          
+    ([2, 4, 6], 99),          
     ([11, 13, 15], 3)        
 ])
 def test_count_odds(inputs, expected):
